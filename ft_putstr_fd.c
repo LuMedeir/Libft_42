28cd4ec3de7b0936ef0cb349622571a1c4f17051
Luiza Medeiros <126430433+LuMedeir@users.noreply.github.com>
2023-05-23 10:34:30 -0300

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
