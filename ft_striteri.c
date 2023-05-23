b311b170a1cff9fc7c311b0125bb93a506fb7fc5
Luiza Medeiros <126430433+LuMedeir@users.noreply.github.com>
2023-05-23 10:34:55 -0300

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
