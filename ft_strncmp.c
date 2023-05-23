c013a30f570b4eab0d8dd5586b6e0face23d62d0
Luiza Medeiros <126430433+LuMedeir@users.noreply.github.com>
2023-05-23 10:35:34 -0300

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
